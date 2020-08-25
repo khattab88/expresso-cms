65854a73cb5b640ba54a9bacdeec1e1b9dea8d55
khattab88 <m_khatab_88@hotmail.com>
2020-08-25 02:48:55 +0200

    use pug template

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.status(200).render("base", {
         name: "Expresso",
-        title: "expresso CMS",
+        title: "expresso cms",
         desc: "CMS system for Expresso project."
     });
 });
