216e446545fe90a5b0427c1fda598b5acfed4125
khattab88 <m_khatab_88@hotmail.com>
2020-09-23 23:50:18 +0200

    install and use models package

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+// use utils package
 const utils = require('expresso-utils');
-
 utils.helpers.echo("khattab");
+
+// use models package
+const { Tag } = require('expresso-models');
+const tag = new Tag();
+console.log(tag.constructor);
