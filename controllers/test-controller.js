504721950989db7bf4ff5cdb7b4fefb14b9a4573
khattab88 <m_khatab_88@hotmail.com>
2021-01-11 16:55:12 +0200

    send password reset email

--- controllers/test-controller.js
+++ controllers/test-controller.js
@@ -4,8 +4,14 @@ const Email = require("../utils/email");
 
 
 exports.test = catchAsync(async (req, res, next) => {
-    const url = `${req.protocol}://${req.get("host")}/login`;
-    await new Email(req.user, url).sendWelcome();
+    /// send WELCOME email
+    // const url = `${req.protocol}://${req.get("host")}/login`;
+    // await new Email(req.user, url).sendWelcome();
+
+    /// send PASSWORD RESET email
+    // const url = `${req.protocol}://${req.get("host")}/login`;
+    await new Email(req.user, "#").sendPasswordReset();
+
 
     res.send("yes");
 });
